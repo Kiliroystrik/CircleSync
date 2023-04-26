5cb2861ef3c2265fbf99c984e11919fd63a6e7c7
Kiliroy <borges.mathieu@gmail.com>
2023-04-26 11:16:14 +0200

    Fixed Html Created GroupController

---
+++ src/Form/GroupType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GroupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('users', EntityType::class, [
+                'class' => 'App\Entity\User',
+                'choice_label' => 'email',
+                'multiple' => true
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Group::class,
+        ]);
+    }
+}
