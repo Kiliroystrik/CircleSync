22da9117f0b39482665626feb67a1c5a7deac8cd
Kiliroy <borges.mathieu@gmail.com>
2023-04-26 09:59:13 +0200

    Edited Scss Created home, registration, and user templates

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -64,6 +66,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->groupes = new ArrayCollection();
         $this->commentaries = new ArrayCollection();
         $this->likes = new ArrayCollection();
+        $this->subscribedDate = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
